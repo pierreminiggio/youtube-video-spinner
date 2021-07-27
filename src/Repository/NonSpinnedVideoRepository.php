d37522546b078211b83d07bba56a64a3607a2cc6
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-07-28 00:12:45 +0200

    exclude elon' shorts

--- src/Repository/NonSpinnedVideoRepository.php
+++ src/Repository/NonSpinnedVideoRepository.php
@@ -22,11 +22,35 @@ class NonSpinnedVideoRepository
         );
         $spinnedContentIds = array_map(fn ($entry) => (int) $entry['id'], $spinnedContentIds);
 
+        $isElonChannel = $youtubeChannelId === 'UCCh4AtUCAhIbmOZbAbB-AhA';
+        if ($isElonChannel) {
+            // Elon Musk Addict' Shorts are posted from TikTok to Youtube.
+            // So we don't need to spin them again to TikTok
+            // So yeah, that's why I exclude them
+            $shortsIds = $this->fetcher->query(
+                $this->fetcher->createQuery(
+                    'youtube_video'
+                )->select(
+                    'id'
+                )->where(
+                    'channel_id = :channel_id
+                    AND description like \'%Shorts%\''
+                ),
+                ['channel_id' => $youtubeChannelId]
+            );
+
+            $shortsIds = array_map(fn ($entry) => (int) $entry['id'], $shortsIds);
+        }
+
         $query = $this->fetcher
             ->createQuery('youtube_video as y')
             ->select('y.id, y.title, y.url')
             ->where('y.channel_id = :channel_id' . (
                 $spinnedContentIds ? ' AND scyv.id IS NULL' : ''
+            ) . (
+                $isElonChannel && $shortsIds
+                    ? (' AND y.id NOT IN (' . implode(', ', $shortsIds) . ')')
+                    : ''
             ))
         ;
 
